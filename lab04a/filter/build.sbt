473729e72a6198759c422e64c8e2c4aa655e691c
Danila Logunov <danilalogunov13@gmail.com>
2022-04-12 15:58:40 +0300

    lab04a

---
+++ lab04a/filter/build.sbt
@@ -0,0 +1,13 @@
+ThisBuild / version := "0.1"
+
+ThisBuild / scalaVersion := "2.11.12"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "filter"
+  )
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.8"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10
+libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.5"
+
---
+++ lab04a/filter/src/main/scala/filter.scala
@@ -0,0 +1,50 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+class filter {
+  def sparkJob(): Unit = {
+    val spark: SparkSession = SparkSession
+                                  .builder()
+                                  .appName("lab04a")
+                                  .getOrCreate()
+    val kafkaOptions = Map("kafka.bootstrap.servers" -> "spark-master-1:6667",
+                            "subscribe" -> spark.conf.get("spark.filter.topic_name"),
+                            "startingOffsets" -> spark.conf.get("spark.filter.offset"),
+                            "maxOffsetsPerTrigger" -> "30",
+                            "minPartitions" -> "5")
+    val df = spark.read.format("kafka").options(kafkaOptions).load
+
+    val schema = StructType(Seq(
+      StructField("event_type", StringType, true), StructField("category", StringType, true),
+      StructField("item_id", StringType, true), StructField("item_price", IntegerType, true),
+      StructField("uid", StringType, true), StructField("timestamp", LongType, true)
+    ))
+
+    val formattedDf = df
+      .select(col("value").cast("string"))
+      .withColumn("value", from_json(col("value"), schema))
+      .select("value.*")
+      .withColumn("date", date_format(from_unixtime(col("timestamp") / 1000), "yyyyMMdd"))
+      .withColumn("p_date", col("date"))
+
+    val path = spark.conf.get("spark.filter.output_dir_prefix")
+
+    formattedDf.filter(col("action_type") === "view")
+        .write
+        .format("json")
+        .option("path", s"/user/danila.logunov/${path}/view")
+        .partitionBy("p_date")
+        .save
+
+    formattedDf.filter(col("event_type") === "buy")
+      .write
+      .format("json")
+      .option("path", s"/user/danila.logunov/${path}/buy")
+      .partitionBy("p_date")
+      .save
+
+    spark.stop()
+  }
+  sparkJob()
+}
