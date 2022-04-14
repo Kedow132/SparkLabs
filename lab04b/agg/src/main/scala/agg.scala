a58a56f6c8fb2ce86d4c7587d0ed38dc4e29416b
Danila Logunov <danilalogunov13@gmail.com>
2022-04-14 13:53:12 +0300

    lab04b

--- lab04b/agg/src/main/scala/agg.scala
+++ lab04b/agg/src/main/scala/agg.scala
@@ -13,10 +13,7 @@ object agg {
 
     val kafkaOptions: Map[String, String] = Map(
       "kafka.bootstrap.servers" -> "spark-master-1:6667",
-      "subscribe" -> "danila_logunov",
-      "startingOffsets" -> "earliest",
-      "maxOffsetsPerTrigger" -> "30",
-      "minPartitions" -> "5"
+      "subscribe" -> "danila_logunov"
     )
 
     val schema: StructType = StructType(Seq(
@@ -36,12 +33,12 @@ object agg {
       .withColumn("value", from_json(col("value"), schema))
       .select("value.*")
       .withColumn("timestamp", (col("timestamp")  / 1000).cast("timestamp"))
-      .withWatermark("timestamp", "1 hours")
       .groupBy(window(col("timestamp"), "1 hours").alias("wnd"))
       .agg(
-        sum(when(col("event_type") === "buy", col("item_price"))).alias("revenue"),
+        sum(when(col("event_type") === "buy", col("item_price"))
+                      .otherwise(0)).alias("revenue"),
         sum(when(col("uid").isNotNull, 1).otherwise(0)).alias("visitors"),
-        count(when(col("event_type") === "buy", 1)).alias("purchases")
+        count(when(col("event_type") === "buy", 1).otherwise(0)).alias("purchases")
       )
       .select(to_json(struct(
         col("wnd.start").cast("long").alias("start_ts"),
@@ -50,19 +47,20 @@ object agg {
         col("visitors"),
         col("purchases"),
         (col("revenue").cast("float") / col("purchases").cast("float")).alias("aov")
-      )).alias("json"))
+      )).alias("value"))
 
 
     val writeDf = formattedDf
       .writeStream
-      .trigger(Trigger.ProcessingTime("5 seconds"))
       .format("kafka")
-      .option("checkpointLocation", "/tmp/chk/lab04b")
-      .option("kafka.bootstrap.server", "10.0.0.5:6667")
-      .option("topic", "danila_logunov_lab04b_out")
-      .option("maxOffsetPerTrigger", 200)
+      .trigger(Trigger.ProcessingTime("5 seconds"))
       .outputMode("update")
-      .start()
+      .option("checkpointLocation", "/user/danila.logunov/tmp/chk/lab04b")
+      .option("kafka.bootstrap.servers", "spark-master-1:6667")
+      .option("topic", "danila_logunov_lab04b_out")
+      .option("maxOffsetsPerTrigger", 200)
+
+    writeDf.start().awaitTermination()
 
   }
 }
